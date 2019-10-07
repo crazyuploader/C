436993c30e45dbac39e0c7962d87b36e52c98cb0
Jugal Kishore <49350241+crazyuploader@users.noreply.github.com>
2019-10-07 14:52:00 +0530

    initial

---
+++ DataStructure/double_link_list.c
@@ -0,0 +1,140 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *left;  //Pointer to left node
+    struct node *right; //Pointer to Right node
+};
+
+struct node *START;
+struct node *createnode()
+{
+    struct node *n;
+    n=(struct node*)malloc(sizeof(struct node));
+    return n;
+};
+
+void insert_begin()
+{
+    struct node *pointer;
+    int item;
+    pointer=createnode();
+    printf("\nEnter a value for a node: ");
+    scanf("%d",&item);
+    if(START=NULL)
+    {
+        pointer->left=NULL;
+        pointer->right=NULL;
+        pointer->data=item;
+        START=pointer;
+    }
+    else
+    {
+        pointer->data=item;
+        pointer->left=NULL;
+        pointer->right=START;
+        START=pointer;
+    }
+    printf("\nNode inserted at Address = %d and its value = %d",pointer,item);
+}
+
+void insert_last()
+{
+    struct node *pointer,*temp;
+    int item;
+    pointer=createnode();
+    printf("\nEnter value for node: ");
+    scanf("%d",&item);
+    pointer->data=item;
+    if(START==NULL)
+    {
+        pointer->right=NULL;
+        pointer->left=NULL;
+        START=pointer;
+    }
+    else
+    {
+        temp=START;
+        while(temp->right!=NULL)
+        {
+            temp=temp->right;
+        }
+        temp->right=pointer;
+        pointer->left=temp;
+        pointer->right=NULL;
+    }
+    printf("\nNode inserted at Address = %d and its value = %d",pointer,item);
+}
+
+void insert_specified()
+{
+    struct node *pointer,*temp;
+    int loc,i,item;
+    pointer=createnode();
+    temp=START;
+    printf("\nEnter location after which you want to add a Node: ");
+    scanf("%d",&loc);
+    for(i=0;i<loc;i++)
+    {
+        temp=temp->right;
+        if(temp==NULL)
+        {
+            printf("\nThere are less than %d element(s)",loc);
+        }
+    }
+    pointer->data=item;
+    pointer->right=temp->right;
+    pointer->left=temp;
+    temp->right=pointer;
+    temp->right->left=pointer;
+    printf("\nNode inserted at Address = %d and its value = %d",pointer,item);
+}
+
+void display()
+{
+    struct node *pointer;
+    printf("Printing values...");
+    pointer=START;
+    while(pointer!=NULL)
+    {
+        printf("\n%d",pointer->data);
+        pointer=pointer->right;
+    }
+}
+
+void main()
+{
+    int choice;
+    printf("///Program for Double Linked-List///");
+    while(1)
+    {
+        printf("\n\nChoose an option from the following list\n");
+        printf("========================================\n");
+        printf("1: Insert a Node at beginning\n");
+        printf("2: Insert a Node at last\n");
+        printf("3: Insert a Node after specified location\n");
+        printf("4: Display the values of every node\n");
+        printf("Anything else to exit!\n");
+        printf("\nchoice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:  insert_begin();
+                     break;
+            case 2:  insert_last();
+                     break;
+            case 3:  insert_specified();
+                     break;
+            case 4:  display();
+                     break;
+            default: printf("Exiting...\n\n");
+                     printf("\t =================================\n");
+                     printf("\t||Created by Jugal Kishore -- 2019||\n");
+                     printf("\t =================================\n");
+                     exit(0);
+                     break;
+        }
+    }
+}
---
+++ DataStructure/link_list.c
@@ -0,0 +1,165 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* link;
+};
+
+struct node *START=NULL;
+struct node* createnode()
+{
+    struct node *n;
+    n=(struct node*)malloc(sizeof(struct node));
+    return n;
+}
+
+void insertNode()
+{
+    struct node *temp, *t;
+    temp=createnode();
+    printf("Enter a value: ");
+    scanf("%d",&temp->data);
+    temp->link=NULL;
+    if(START==NULL)
+    {
+        START=temp;
+        printf("Node is created at Address = %d and its value is = %d",temp,temp->data);
+    }
+    else
+    {
+        t=START;
+        while(t->link!=NULL)
+        {
+        t=t->link;
+        }
+        t->link=temp;
+        printf("Node is created at Address = %d and its value is = %d",temp,temp->data);
+    }
+}
+
+void deleteNode()
+{
+    struct node *r;
+    if(START==NULL)
+    {
+        printf("Linked-List is empty!");
+    }
+    else
+    {
+        r=START;
+        while(r!=NULL)
+        {
+            printf("Node Address is = %d and its value is =",r,*r);
+            free(r);
+        }
+    }
+}
+
+void viewNode()
+{
+    struct node *t;
+    if(START==NULL)
+    {
+        printf("Linked-List is empty!");
+    }
+    else
+    {
+        t=START;
+        while(t!=NULL)
+        {
+        printf("\nNode Address is = %d and its value is = %d",t,t->data);
+        t=t->link;
+        }
+    }
+}
+
+int length()
+{
+    struct node *t;
+    int len=0;
+    if(START==NULL)
+    {
+        printf("Linked-List is empty!");
+        return 0;
+    }
+    else
+    {
+        t=START;
+        while(t!=NULL)
+        {
+        t=t->link;
+        len=len++;
+        }
+        return len;
+    }
+}
+
+void deleteLocation()
+{
+    struct node *t, *p, *q;
+    int loc,i=1;
+    printf("Enter location you want to delete node at: ");
+    scanf("%d",&loc);
+    if(loc>length())
+    {
+        printf("Invalid Location");
+    }
+    else
+    {
+        if(loc==1)
+        {
+            t=START;
+            START=t->link;
+            t->link=NULL;
+        }
+        else
+        {
+            p=START;
+            while(i<loc-1)
+            {
+                p=p->link;
+                i++;
+            }
+            q=p->link;
+            p->link=q->link;
+            q->link=NULL;
+            free(q);
+        }
+    }
+}
+
+void main()
+{
+    int choice;
+    printf("///Linked-List Program///");
+    while(1)
+    {
+        printf("\n\n 1: Insert a Node");
+        printf("\n 2: Delete a Node");
+        printf("\n 3: View every Node");
+        printf("\n 4: Delete Node at a particular location");
+        printf("\n 5: Exit");
+        printf("\n\nEnter a choice: ");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:  insertNode();
+                     break;
+            case 2:  deleteNode();
+                     break;
+            case 3:  viewNode();
+                     break;
+            case 4:  deleteLocation();
+                     break;
+            case 5:  printf("Exiting...\n\n");
+                     printf("\t =================================\n");
+                     printf("\t||Created by Jugal Kishore -- 2019||\n");
+                     printf("\t =================================\n");
+                     exit(0);
+                     break;
+            default: printf("Invalid Choice entered.");
+        }
+    }
+}
