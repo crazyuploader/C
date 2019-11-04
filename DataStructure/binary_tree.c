dae89f492a61ac9783124dd6d4de1eb911765a26
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2019-11-04 15:36:55 +0530

    adding Practical File

--- DataStructure/binary_tree.c
+++ DataStructure/binary_tree.c
@@ -1,7 +1,55 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+};
+
+struct node *root=NULL;
+
+void insert(int d)
+{
+    struct node *t,*parent;
+    t=(struct node*)malloc(sizeof(struct node));
+    t->data=d;
+    t->left=t->right=NULL;
+    parent=root;
+    if(root==NULL)
+    {
+        root=t;
+    }
+    else
+    {
+        struct node *current;
+        current=root;
+        while(current)
+        {
+            parent=current;
+            if(t->data>current->data)
+            {
+                current=current->right;
+            }
+            else
+            {
+                current=current->left;
+            }
+        }
+    }
+    if(t->data>parent->data)
+    {
+        parent->right=t;
+    }
+    else
+    {
+        parent->left=t;
+    }
+}
+
 int main()
 {
-
+    printf("...Binary Tree...\n");
+    printf("")
 }
---
+++ Practical_File/README.md
@@ -0,0 +1,6 @@
+Practical File for Data Structure using C
+---
+
+[![Build Status](https://travis-ci.org/crazyuploader/C.svg?branch=master)](https://travis-ci.org/crazyuploader/C)
+
+My Practical File for Data Structure using C which also can be run online [HERE](https://practicalc.jugalkishore.repl.run/).
