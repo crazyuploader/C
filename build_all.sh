a752d4432d27fbf8961b24628114dc41daedbc16
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-01-15 22:30:47 +0530

    Fix: build script

--- build_all.sh
+++ build_all.sh
@@ -3,14 +3,14 @@
 echo ""
 echo "Compiling with GCC"
 echo ""
-for f in $(ls *.c); do echo "Compiling $f"; gcc $f; done
+for f in *.c; do echo "Compiling $f"; gcc "$f"; done
 echo ""
 echo "Build with GCC OK"
 echo ""
 echo ""
 echo "Compiling with Clang"
 echo ""
-for f in $(ls *.c); do echo "Compiling $f"; clang $f; done
+for f in *.c; do echo "Compiling $f"; clang "$f"; done
 echo ""
 echo "Build with Clang OK"
 echo ""
