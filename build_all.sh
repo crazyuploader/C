7635e2ac3cc328d4f90f8d9c72fb8fb0d202be8f
crazyuploader <49350241+crazyuploader@users.noreply.github.com>
2020-04-13 21:49:26 +0530

    Update: build_all.sh

--- build_all.sh
+++ build_all.sh
@@ -1,16 +1,18 @@
 #!/usr/bin/env bash
 
+# Colors
+GREEN="\033[1;32m"
+NC="\033[0m"
+YELLOW="\033[1;33m"
+MAGENTA="\033[1;35m"
+
+clear
+echo -e "${GREEN}Compiling with GCC${NC}"
 echo ""
-echo "Compiling with GCC"
+for f in *.c; do echo "Compiling '${f}'"; gcc "${f}"; done
 echo ""
-for f in *.c; do echo "Compiling $f"; gcc "$f"; done
+echo -e "${GREEN}Compiling with Clang${NC}"
 echo ""
-echo "Build with GCC OK"
+for f in *.c; do echo "Compiling '${f}'"; clang "${f}"; done
 echo ""
-echo ""
-echo "Compiling with Clang"
-echo ""
-for f in *.c; do echo "Compiling $f"; clang "$f"; done
-echo ""
-echo "Build with Clang OK"
-echo ""
+echo -e "${MAGENTA}Done!${NC}"
